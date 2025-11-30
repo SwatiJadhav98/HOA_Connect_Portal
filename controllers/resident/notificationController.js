e035e01619e15fe29ac20a483abaabede42d5957
priya-1008 <pinkyrathod0803@gmail.com>
2025-11-30 18:19:25 +0530

    payment receipt and polls route added

---
+++ controllers/resident/notificationController.js
@@ -0,0 +1,30 @@
+const User = require("../../models/User");
+const Notification = require("../../models/Notification");
+
+exports.getNotification = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const communityId = req.user.community;
+
+    const notifications = await Notification.find({
+      community: communityId,
+      recipients: userId, // only if user is a recipient
+    })
+      .populate("createdBy", "name role")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({
+      success: true,
+      count: notifications.length,
+      notifications,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch notifications",
+      error: error.message,
+    });
+  }
+};
